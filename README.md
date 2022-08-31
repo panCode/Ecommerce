b23d1089420fb7e0b6ac07858852cdb79b906fea
NikhilPandey <nikhil.pandey@oyorooms.com>
2022-09-01 00:10:33 +0530

    updated read me

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Ecommerce
+Steps to run
+1. clone this repo
+2. npm install
+3. npm run dev
+4. the localserver will start on localhost:8080
+5. open http://localhost:8080/ on the browser
--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   entry: path.join(__dirname, "src", "app.js"),
   output: {
     path:path.resolve(__dirname, "dist"),
